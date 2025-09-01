a47f8c6af38fa7c094a5a995f003254b4440ccd1
Rizqi Arif <rizkyarif0106@gmail.com>
2025-09-01 11:50:10 +0700

    first commit

---
+++ while-loop-table.php
@@ -0,0 +1,15 @@
+<table border="1">
+    <th>NO</th>
+    <th>NAMA</th>
+    <?php
+    $i = 1;
+    while ($i < 100) {
+        echo "<tr>
+            <td>" . $i . "</td>
+            <td>rizqi " . $i . "</td>
+        </tr>";
+        $i++;
+    }
+    ?>
+
+</table>
